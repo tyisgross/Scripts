ccaf837662247f43cd3e96608eb21bf2842a39ae
tyisgross <158226333+tyisgross@users.noreply.github.com>
2024-12-09 10:48:26 -0800

    Update AD Offboarding - Glostone.ps1

--- AD Offboarding - Glostone.ps1
+++ AD Offboarding - Glostone.ps1
@@ -1,28 +1,29 @@
 # update this for each user
 $userlogin = "carolynk"
-$useremail = $userlogin + "@glostone.com"
+
 
 # constant variables
-$o365 = "f245ecc8-75af-4f8e-b61f-27d8114de5f3"
-$mde = "5e1e7702-a2b7-4360-8d07-2f515792896f"
-$atp = "4ef96642-f096-40de-a3e9-d83fb2f90211"
+$useremail = $userlogin + "@glostone.com"
+$o365 = "############-75af-4f8e-b61f-############"
+$mde = "############-a2b7-4360-8d07-############"
+$atp = "############-f096-40de-a3e9-############"
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+$credential = Get-Credential
 #Import-Module ExchangeOnlineManagement
 #Import-Module Microsoft.Graph
 Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All
-Connect-ExchangeOnline -UserPrincipalName tyg@glostone.com
-Connect-MicrosoftTeams -UserPrincipalName tyg@glostone.com
+Connect-ExchangeOnline -Credential $credential
+Connect-MicrosoftTeams -Credential $credential
 
 # Run this stuff first
-
-Remove-DistributionGroupMember -Identity AllGlostone -Member $useremail -Confirm:$false
-Remove-DistributionGroupMember -Identity AllUsers -Member $useremail -Confirm:$false
-Remove-DistributionGroupMember -Identity AllHands -Member $useremail -Confirm:$false
+Remove-ADGroupMember -Group "Glostone Usr" -Member $userlogin -Confirm:$false
+Remove-DistributionGroupMember -Identity "Vehicle Services" -Member $useremail -Confirm:$false
+Remove-DistributionGroupMember -Identity "All Hands" -Member $useremail -Confirm:$false
 Remove-UnifiedGroupLinks -Identity "Vehicle Services Team" -LinkType Members -Links $useremail -Confirm:$false
 Disable-ADAccount -Identity $userlogin
 Set-MgUserLicense -UserId $useremail -RemoveLicenses @($o365) -AddLicenses @{}
 Set-MgUserLicense -UserId $useremail -RemoveLicenses @($mde) -AddLicenses @{}
 Set-MgUserLicense -UserId $useremail -RemoveLicenses @($atp) -AddLicenses @{}
-Remove-TeamUser -user $useremail -GroupId 84b08969-307c-4ae6-8e29-7818839e1a9d -Role Member
-Remove-TeamUser -user $useremail -GroupId 25de6371-623c-4817-9bd5-337184768dfe -Role Member
+Remove-TeamUser -user $useremail -GroupId ############-307c-4ae6-8e29-############ -Role Member
+Remove-TeamUser -user $useremail -GroupId ############-623c-4817-9bd5-############ -Role Member
