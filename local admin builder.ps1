bbce8d9663f4d7cf965be618f7a327728ae1349c
tyisgross <158226333+tyisgross@users.noreply.github.com>
2024-12-09 10:57:26 -0800

    Update local admin builder.ps1

--- local admin builder.ps1
+++ local admin builder.ps1
@@ -1,6 +1,6 @@
 ﻿##This script should be run with a GPO to build a local admin account on computer startup so that the IT team always has a backdoor.
 
 $username = "gstadmin"
-$password = ConvertTo-SecureString "GSTL0c@!adm12" -AsPlainText -Force
+$password = ConvertTo-SecureString "asdfqwerasdf" -AsPlainText -Force
 New-LocalUSer -Name "$username" -Password $password -FullName "$username" -Description "Local Admin"
 Add-LocalGroupMember -Group "Administrators" -Member "$username"
