fc160fa991511db13f49182754123868dd6551fb
tyisgross <158226333+tyisgross@users.noreply.github.com>
2024-12-09 16:11:50 -0800

    Update AD Offboarding - Cleanfleet.ps1

--- AD Offboarding - Cleanfleet.ps1
+++ AD Offboarding - Cleanfleet.ps1
@@ -1,19 +1,23 @@
 # update this for each user
-$userlogin = "testingg"
+$userlogin = "testingt"
 
 # constant variables
 $useremail = $userlogin + "@cleanfleet.org"
-$o365 = "############-75af-4f8e-b61f-############"
-$mde = "############-a2b7-4360-8d07-############"
-$atp = "############-f096-40de-a3e9-############"
+$o365 = "f245ecc8-75af-4f8e-b61f-27d8114de5f3"
+$mde = "5e1e7702-a2b7-4360-8d07-2f515792896f"
+$atp = "4ef96642-f096-40de-a3e9-d83fb2f90211"
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 $credential = Get-Credential
-#Import-Module ExchangeOnlineManagement
-#Import-Module Microsoft.Graph
-Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All
+Import-Module -UseWindowsPowerShell -Name ADSync;
+Start-ADSyncSyncCycle -PolicyType Initial
+Import-Module Microsoft.Graph.Entra
+
+# Run this stuff to sync with the online office365 portal
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
 Connect-ExchangeOnline -Credential $credential
 Connect-MicrosoftTeams -Credential $credential
+Start-Sleep -Seconds 45
 
 # These things actually do the work; Removing licenses, distribution lists, teams groups, 
 Remove-ADGroupMember -Identity "CleanFleet Usr" -Member $userlogin -Confirm:$false
@@ -24,8 +28,7 @@ Disable-ADAccount -Identity $userlogin
 Set-MgUserLicense -UserId $useremail -RemoveLicenses @($o365) -AddLicenses @{}
 Set-MgUserLicense -UserId $useremail -RemoveLicenses @($mde) -AddLicenses @{}
 Set-MgUserLicense -UserId $useremail -RemoveLicenses @($atp) -AddLicenses @{}
-Remove-TeamUser -user $useremail -GroupId ############-5760-481e-b2bc-############ -Role Member
-Remove-TeamUser -user $useremail -GroupId ############-623c-4817-9bd5-############ -Role Member
+Remove-TeamUser -user $useremail -GroupId b5e025d7-5760-481e-b2bc-d3467db9353d -Role Member
+Remove-TeamUser -user $useremail -GroupId 25de6371-623c-4817-9bd5-337184768dfe -Role Member
 
 #remember to check work :)
-
