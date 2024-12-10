974d95725babbe197a7183c1ecb0e40bc71c869d
tyisgross <158226333+tyisgross@users.noreply.github.com>
2024-12-09 16:11:22 -0800

    Update AD Onboarding - Cleanfleet.ps1

--- AD Onboarding - Cleanfleet.ps1
+++ AD Onboarding - Cleanfleet.ps1
@@ -1,16 +1,16 @@
 # update this for each user
-$userlogin = "testingg"
+$userlogin = "testingt"
 $firstname = "testing"
-$lastname = "gross"
+$lastname = "ty"
 
 
 #constants
-$password = "Sample Text!"
+$password = "Gradient2025!"
 $ppassword = $password | ConvertTo-SecureString -AsPlainText -Force
 $useremail = $userlogin + "@cleanfleet.org"
-$o365 = "###########-75af-4f8e-b61f-###########"
-$mde = "###########-a2b7-4360-8d07-###########"
-$atp = "###########-f096-40de-a3e9-###########"
+$o365 = "f245ecc8-75af-4f8e-b61f-27d8114de5f3"
+$mde = "5e1e7702-a2b7-4360-8d07-2f515792896f"
+$atp = "4ef96642-f096-40de-a3e9-d83fb2f90211"
 
 #creates user locally
 $splat = @{
@@ -29,19 +29,20 @@ Add-ADGroupMember -Identity "CleanFleet Usr" -Members $userlogin -Confirm:$false
 
 Start-Sleep -Seconds 10
 
+$credential = Get-Credential
+Import-Module -UseWindowsPowerShell -Name ADSync;
 Start-ADSyncSyncCycle -PolicyType Initial
+Import-Module Microsoft.Graph.Entra
 
 # Run this stuff to sync with the online office365 portal
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-$credential = Get-Credential
-Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
 Connect-ExchangeOnline -Credential $credential
 Connect-MicrosoftTeams -Credential $credential
-Import-Module -UseWindowsPowerShell -Name ADSync;
-Start-Sleep -Seconds 60
-
+Start-Sleep -Seconds 45
 
 # These things actually do the work; Setting licenses, distribution lists, teams groups.
+Set-EntraUser -UserId $useremail -UsageLocation "US"
 Set-MgUserLicense -UserId $useremail -AddLicenses @{SkuID = $o365} -RemoveLicenses @()
 Set-MgUserLicense -UserId $useremail -AddLicenses @{SkuID = $mde} -RemoveLicenses @()
 Set-MgUserLicense -UserId $useremail -AddLicenses @{SkuID = $atp} -RemoveLicenses @()
@@ -49,5 +50,5 @@ Add-DistributionGroupMember -Identity "All Cleanfleet" -Member $useremail
 Add-DistributionGroupMember -Identity "All Hands" -Member $useremail
 Add-UnifiedGroupLinks -Identity "Driver Services Team" -LinkType Members -Links $useremail -Confirm:$false
 Add-UnifiedGroupLinks -Identity "All Hands Team" -LinkType Members -Links $useremail -Confirm:$false
-Add-TeamUser -user $useremail -GroupId ###########-5760-481e-b2bc-########### -Role Member
-Add-TeamUser -user $useremail -GroupId ###########-623c-4817-9bd5-########### -Role Member
+Add-TeamUser -user $useremail -GroupId b5e025d7-5760-481e-b2bc-d3467db9353d -Role Member
+Add-TeamUser -user $useremail -GroupId 25de6371-623c-4817-9bd5-337184768dfe -Role Member
